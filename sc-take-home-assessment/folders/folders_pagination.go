8b9bab72583fb8d78803f7d9eda4af55bea3eaa3
JayCod3r <84632172+JayCod3r@users.noreply.github.com>
2022-10-17 14:58:54 +1100

    Created Pagination
    Made the pagination function,

--- sc-take-home-assessment/folders/folders_pagination.go
+++ sc-take-home-assessment/folders/folders_pagination.go
@@ -2,7 +2,6 @@ package folders
 
 import (
 	"errors"
-	"fmt"
 
 	"github.com/gofrs/uuid"
 )
@@ -33,8 +32,72 @@ import (
 func pagination() {
 	// The aim is to group up all the folders in twos and pair them with a token. Also Have a token that directs them towards the next set.
 	// If I put a loop and have a map with a key. and it will put two folders into each segement then have a key. to access that area in the map.
-	fmt.Println("hello")
+	// f := []Folder{}
+	// r, _ := FetchAllFoldersByOrgID(req.OrgID)
+	// for k, v := range r { // for , pointer to folder, in a slice of pointers to folder.
+	// 	f = append(f, *v) // this appends *v to the slice f
+	// 	k++
+	// }
 
+	// var token int
+	// for i := 0; i <= 5; i++ {
+
+	// 	fmt.Println("Choose a token between 0 and 499")
+	// 	fmt.Scanln(&token)
+
+	// 	if token <= -2 || token >= 500 { // This is because The tokens start at 0-499 as 0 is the first one. so there are still 500 token
+	// 		// Also I don't want them to type in a negative integer as it would bring up none of the entries.
+
+	// 		fmt.Println("Error: Invalid Token number")
+
+	// 	} else if token == -1 { // Making this the exit point
+	// 		fmt.Println("___________________________________________________")
+	// 		fmt.Println("Thank you")
+	// 		fmt.Println("___________________________________________________")
+	// 		var fetchResponse *FetchFolderResponse
+	// 		return fetchResponse, nil // returns to end // This section is a WIP
+	// 	} else if token == 0 { // Exception 1 if the token is 0 it would need a different system as it is an outlier.
+	// 		fmt.Println("___________________________________________________")
+	// 		fmt.Print("Token: ")
+	// 		fmt.Println(token)
+	// 		fmt.Print("Folder entry 1: ")
+	// 		fmt.Println(f[token])
+	// 		fmt.Print("Folder entry 2: ")
+	// 		fmt.Println(f[token+1])
+	// 		fmt.Println("Previous Token: No Previous Token")
+	// 		fmt.Println("Next Token: 1")
+	// 		fmt.Println("Enter -1 to end")
+	// 		fmt.Println("___________________________________________________")
+	// 	} else if token == 499 { // Exception 2 as the final token has differences.
+	// 		fmt.Println("___________________________________________________")
+	// 		fmt.Print("Token: ")
+	// 		fmt.Println(token)
+	// 		fmt.Println("Folder entry 1: ")
+	// 		fmt.Println(f[token*2])
+	// 		fmt.Print("Previous Token: ")
+	// 		fmt.Println(token - 1)
+	// 		fmt.Print("Next Token: No Next Token")
+	// 		fmt.Println("___________________________________________________")
+	// 	} else {
+	// 		fmt.Println("___________________________________________________")
+	// 		fmt.Print("Token: ")
+	// 		fmt.Println(token)
+	// 		fmt.Print("Folder entry 1:")
+	// 		fmt.Println(f[token*2])
+	// 		fmt.Print("Folder entry 2:")
+	// 		fmt.Println(f[token*2+1])
+	// 		fmt.Print("Previous Token: ")
+	// 		fmt.Println(token - 1)
+	// 		fmt.Print("Next Token: ")
+	// 		fmt.Println(token + 1)
+	// 		fmt.Println("Enter -1 to end")
+	// 		fmt.Println("___________________________________________________")
+	// 	}
+
+	// }
+
+	// var fetchResponse *FetchFolderResponse
+	// return fetchResponse, nil
 }
 
 func ModGetAllFolders(req *FetchFolderRequest) (*FetchFolderResponse, error) { // This is my modified version of GetAllFolder
